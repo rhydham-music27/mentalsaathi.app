e358e5f386f55bd2a42d56fb906f92491789bd02
rhydham-music27 <rhydham937@gmail.com>
2025-06-09 09:30:55 +0530

    sitemap

--- scripts/sitemap.js
+++ scripts/sitemap.js
@@ -5,7 +5,7 @@ SitemapGenerator({
   pagesDirectory: __dirname + "/../app", // your app router folder
   targetDirectory : __dirname +  '/../public/',
   ignoredExtensions: ['png', 'jpg'],
-  ignoredPaths: ['[id]', 'admin','test'],
+  ignoredPaths: ['[id]', 'admin','admin/*','test/*','test'],
   nextConfigPath: __dirname + "/../next.config.mjs"
 
 })
