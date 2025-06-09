1c72c7c62fbae23be02e1aa0593c46548f6fcbaa
rhydham-music27 <rhydham937@gmail.com>
2025-06-09 09:21:38 +0530

    cretaed sitemaps

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://mentalsaathi.in', // your live domain
+  generateRobotsTxt: true,
+
+};
---
+++ scripts/sitemap.js
@@ -0,0 +1,11 @@
+const SitemapGenerator = require('nextjs-sitemap-generator');
+
+SitemapGenerator({
+  baseUrl: 'https://mentalsaathi.in',
+  pagesDirectory: __dirname + "/../app", // your app router folder
+  targetDirectory : __dirname +  '/../public/',
+  ignoredExtensions: ['png', 'jpg'],
+  ignoredPaths: ['[id]', 'admin','test'],
+  nextConfigPath: __dirname + "/../next.config.mjs"
+
+})
