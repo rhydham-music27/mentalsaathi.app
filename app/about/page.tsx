cd09176bc1cc77e0454d069b89766351fb5d1bb7
rhydham-music27 <rhydham937@gmail.com>
2025-06-10 15:46:49 +0530

    active

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Navbar } from "@/components/layout/navbar";
 import { Footer } from "@/components/layout/footer";
